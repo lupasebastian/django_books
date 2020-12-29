5565d386e37dbd5a5dc763bfac7c47d97c75f70c
Sebastian <lupasebastian@gmail.com>
2020-12-29 15:27:51 +0100

    buttons moved to a sidebar

--- library/accounts/forms.py
+++ library/accounts/forms.py
@@ -1,3 +1,4 @@
+"""module defining form for creating new users"""
 from django.contrib.auth.forms import UserCreationForm
 from django.db.transaction import atomic
 from django.forms import CharField, Textarea
@@ -5,12 +6,17 @@ from django.forms import CharField, Textarea
 from .models import Profile
 
 
-class SignUpForm(UserCreationForm):
+# pylint: disable=too-few-public-methods
 
+
+class SignUpForm(UserCreationForm):
+    """form defined for signing new user up"""
     class Meta(UserCreationForm.Meta):
+        """class defining which fields from model should be included"""
         fields = ['username', 'first_name']
 
-    information = CharField(label='Opowiedz parę słów o sobie i ulubionych książkach:', widget=Textarea, min_length=60)
+    information = CharField(label='Opowiedz parę słów o sobie i ulubionych '
+                                  'książkach:', widget=Textarea, min_length=60)
 
     @atomic
     def save(self, commit=True):
--- library/accounts/models.py
+++ library/accounts/models.py
@@ -1,9 +1,10 @@
+"""models to be used in accounts app"""
+
 from django.contrib.auth.models import User
 from django.db.models import Model, OneToOneField, CASCADE, TextField
 
 
-# Create your models here.
-
 class Profile(Model):
+    """class for creating Profile objects while signing up"""
     user = OneToOneField(User, on_delete=CASCADE)
     information = TextField()
